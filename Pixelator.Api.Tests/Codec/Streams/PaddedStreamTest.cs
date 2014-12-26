7dcee97afde095932bd039d97386fec537d75dd1
Elliot Levin <elliot@aanet.com.au>
2014-12-26 20:54:42 +1100

    [Transfer]

---
+++ Pixelator.Api.Tests/Codec/Streams/PaddedStreamTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Pixelator.Api.Codec.Streams;
+
+namespace Pixelator.Api.Tests.Codec.Streams
+{
+    [TestFixture]
+    internal class PaddedStreamTest
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_FailsWithNullInnerStream()
+        {
+            new PaddedStream(null, 0, null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Constructor_FailsOnNegativeRepeatAmount()
+        {
+            new PaddedStream(new MemoryStream(), 0, -1);
+        }
+
+        [Test]
+        [TestCase(new byte[] { 1 }, 5, 1)]
+        [TestCase(new byte[] { 1 }, 0, 3)]
+        [TestCase(new byte[] { 1, 2 }, 200, 1)]
+        [TestCase(new byte[] { 1, 2 }, 40, 5)]
+        [TestCase(new byte[] { 1, 2, 3, 24, 1, 3, 8 }, 20, 15)]
+        public void PaddedStream_PadsInnerStreamData(byte[] data, byte paddingValue, int paddingLength)
+        {
+            PaddedStream paddedStream = new PaddedStream(new MemoryStream(data) { Position = 0 }, paddingValue, paddingLength);
+
+            Assert.AreEqual(0, paddedStream.Position);
+            Assert.AreEqual(data.Length + paddingLength, paddedStream.Length);
+
+            MemoryStream ouputStream = new MemoryStream();
+            paddedStream.CopyTo(ouputStream);
+
+            Assert.AreEqual(paddedStream.Length, paddedStream.Position);
+
+            CollectionAssert.AreEqual(
+                data.Concat(Enumerable.Repeat(paddingValue, paddingLength)).ToArray(),
+                ouputStream.ToArray());
+        }
+
+        [Test]
+        public void PaddedStream_SeekingWorks()
+        {
+            PaddedStream paddedStream = new PaddedStream(
+                new MemoryStream(Enumerable.Range(0, 20).Select(i => (byte)i).ToArray()) { Position = 0 },
+                0,
+                30);
+
+            MemoryStream ouputStream = new MemoryStream();
+            paddedStream.CopyTo(ouputStream);
+
+            Assert.AreEqual(paddedStream.Length, paddedStream.Position);
+
+            CollectionAssert.AreEqual(
+                Enumerable.Range(0, 20).Select(i => (byte)i).Concat(Enumerable.Repeat((byte)0, 30)).ToArray(),
+                ouputStream.ToArray());
+
+            paddedStream.Position = 10;
+            ouputStream = new MemoryStream();
+            paddedStream.CopyTo(ouputStream);
+
+            CollectionAssert.AreEqual(
+                Enumerable.Range(10, 20).Select(i => (byte)i).Concat(Enumerable.Repeat((byte)0, 30)).ToArray(),
+                ouputStream.ToArray());
+        }
+    }
+}
--- Pixelator.Api/Codec/Streams/PaddedStream.cs
+++ Pixelator.Api/Codec/Streams/PaddedStream.cs
@@ -1,16 +1,137 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.IO;
 
 namespace Pixelator.Api.Codec.Streams
 {
-    class PaddedStream : CombinedStream
+    class PaddedStream : Stream
     {
+        private readonly Stream _stream;
+        private readonly byte _paddingValue;
+        private readonly long? _paddingLength;
+        private long _paddingPosition = 0;
+
         public PaddedStream(Stream stream, byte paddingValue, long? paddingLength)
-            : base(new [] { 
-                stream, 
-                paddingLength.HasValue ? (Stream) new SubStream(new ConstantStream(paddingValue), 0, paddingLength.Value) : new ConstantStream(paddingValue), 
-        })
         {
+            if (stream == null)
+            {
+                throw new ArgumentNullException("stream");
+            }
+
+            if (paddingLength < 0)
+            {
+                throw new ArgumentOutOfRangeException("paddingLength", "cannot be less than zero");
+            }
+
+            _stream = stream;
+            _paddingValue = paddingValue;
+            _paddingLength = paddingLength;
+        }
+
+        public override void Flush()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int totalBytesRead = 0;
+
+            while (totalBytesRead < count)
+            {
+                int bytesRead = _stream.Read(buffer, offset + totalBytesRead, count - totalBytesRead);
+
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                totalBytesRead += bytesRead;
+            }
+
+            int paddingBytesCount = _paddingLength.HasValue
+                ? (int)Math.Min((long)_paddingLength - _paddingPosition, count - totalBytesRead) 
+                : count - totalBytesRead;
+            for (int i = 0; i < paddingBytesCount; i++)
+            {
+                buffer[i + offset + totalBytesRead] = _paddingValue;
+            }
+
+            totalBytesRead += paddingBytesCount;
+            _paddingPosition += paddingBytesCount;
+
+            return totalBytesRead;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    Position = offset;
+                    break;
+                case SeekOrigin.Current:
+                    Position += offset;
+                    break;
+                case SeekOrigin.End:
+                    Position = Length + offset;
+                    break;
+            }
+
+            return Position;
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return true; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override long Length
+        {
+            get { return _paddingLength.HasValue ? _stream.Length + _paddingLength.Value : long.MaxValue; }
+        }
+
+        public override long Position
+        {
+            get { return _stream.Position + _paddingPosition; }
+            set
+            {
+                if (value > Length)
+                {
+                    throw new ArgumentOutOfRangeException(null, "Attempted to seek beyond the end of the stream");
+                }
+
+                if (value > _stream.Length)
+                {
+                    _stream.Position = _stream.Length;
+                    _paddingPosition = value - _stream.Length;
+                }
+                else
+                {
+                    _paddingPosition = 0;
+                    _stream.Position = value;
+                }
+            }
         }
     }
 }
